53e308ccc34e675700d853e052b29cc223f251fc
shudv2021 <shudv@yandex.ru>
2021-07-17 14:36:54 +0300

    add by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,8 @@
 class Test < ApplicationRecord
+  def self.by_category(category)
+    category_id = Category.find_by(title:category)
+    pp Test.where(category_id: category_id).order!('id DESC')
 
+    #  Test.joins('JOIN categories ON tests.category_id = categories.id').where("category = :category", category:category)
+  end
 end
