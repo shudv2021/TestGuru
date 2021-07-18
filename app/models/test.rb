41206d6584f0e692bc6e92400e78194fed9183e8
shudv2021 <shudv@yandex.ru>
2021-07-18 11:37:06 +0300

    dont work

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,11 @@
 class Test < ApplicationRecord
   def self.by_category(category)
-    category_id = Category.find_by(title:category)
-    pp Test.where(category_id: category_id).order!('id DESC')
+    #category_id = Category.find_by(title:category)
+    #pp Test.where(category_id: category_id).order!('id DESC')
 
-    #  Test.joins('JOIN categories ON tests.category_id = categories.id').where("category = :category", category:category)
+    pp Test.joins('JOIN categories ON tests.category_id = categories.id')
+           .where("category = :category", category:category)
+           .order('id DESC')
   end
+  
 end
