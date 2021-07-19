57b67e02552313634eb1963cec8dee36922b624d
shudv2021 <shudv@yandex.ru>
2021-07-19 15:11:23 +0300

    correct the seed mistake

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,48 +6,48 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create([
+categories = Category.create([
   {title:'Frontend'},
   {title:'Backend'},
   {title:'Mobile devops'},
   {title:'Data science'}
   ])
 
-Test.create([
-  {title:'Ruby', level:1, category_id:2},
-  {title:'Ruby', level:2, category_id:2},
-  {title:'HTML', level:0, category_id:1},
-  {title:'SCC', level:1, category_id:1},
-  {title:'Go', level:2, category_id:3},
-  {title:'Phyton', level:2, category_id:4}
+tests = Test.create([
+  {title:'Ruby', level:1, category_id:categories[1].id},
+  {title:'Ruby', level:2, category_id:categories[1].id},
+  {title:'HTML', level:0, category_id:categories[0].id},
+  {title:'SCC', level:1, category_id:categories[0].id},
+  {title:'Go', level:2, category_id:categories[2].id},
+  {title:'Phyton', level:2, category_id:categories[3].id}
   ])
-Question.create([
-  {test_id:1, body:'For what Ruby is used usually'},
-  {test_id:3, body:'For what HTML is used'},
-  {test_id:2, body:'Haw chenge values in a & b each other'},
-  {test_id:4, body:'For what CSS is used '},
-  {test_id:5, body:'What common between Ruby and Phyton'}
+questions = Question.create([
+  {test_id:tests[0].id, body:'For what Ruby is used usually'},
+  {test_id:tests[2].id, body:'For what HTML is used'},
+  {test_id:tests[1].id, body:'Haw chenge values in a & b each other'},
+  {test_id:tests[3].id, body:'For what CSS is used '},
+  {test_id:tests[4].id, body:'What common between Ruby and Phyton'}
   ])
 Answer.create([
- {question_id:1, body:'For Backend production', correct:true},
- {question_id:1, body:'For Frontend production', correct:false},
- {question_id:1, body:'For BigData and Analitic', correct:false },
- {question_id:2, body:'For Frontend production', correct:true},
- {question_id:5, body:'Both lenguage with dynamic typing jf data', correct:true},
- {question_id:4, body:'For drawing and decoration HTML pages', correct:true},
+ {question_id:questions[0].id, body:'For Backend production', correct:true},
+ {question_id:questions[0].id, body:'For Frontend production', correct:false},
+ {question_id:questions[0].id, body:'For BigData and Analitic', correct:false },
+ {question_id:questions[1].id, body:'For Frontend production', correct:true},
+ {question_id:questions[4].id, body:'Both lenguage with dynamic typing jf data', correct:true},
+ {question_id:questions[3].id, body:'For drawing and decoration HTML pages', correct:true},
  ])
-User.create([
+users = User.create([
   {name:'Masha', password:'12345', status:'test object'},
   {name:'Glasha', password:'12345', status:'test object'},
   {name:'Pasha', password:'12345', status:'test object'},
   {name:'Veniamin', password:'qwerty', status:'test maker'}
   ])
 TestAction.create([
-  {test_id:1, user_id:1, result:2},
-  {test_id:2, user_id:1, result:2},
-  {test_id:4, user_id:1, result:2},
-  {test_id:1, user_id:2, result:1},
-  {test_id:2, user_id:2, result:1},
-  {test_id:3, user_id:2, result:1},
-  {test_id:4, user_id:3, result:1}
+  {test_id:tests[0].id, user_id:users[0].id, result:2},
+  {test_id:tests[1].id, user_id:users[0].id, result:2},
+  {test_id:tests[3].id, user_id:users[0].id, result:2},
+  {test_id:tests[0].id, user_id:users[1].id, result:1},
+  {test_id:tests[1].id, user_id:users[1].id, result:1},
+  {test_id:tests[2].id, user_id:users[1].id, result:1},
+  {test_id:tests[3].id, user_id:users[2].id, result:1}
   ])
