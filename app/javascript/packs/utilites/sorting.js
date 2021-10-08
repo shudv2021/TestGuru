e7d3e267220bd0c802f8f506e5c24ebc70a18389
shudv2021 <shudv@yandex.ru>
2021-10-08 17:44:16 +0300

    chenge sintaxits

--- app/javascript/packs/utilites/sorting.js
+++ app/javascript/packs/utilites/sorting.js
@@ -1,48 +1,60 @@
 document.addEventListener('turbolinks:load', function (){
-    let control = document.querySelector('.sort-by-title');
-    if (control) {control.addEventListener('click', sortRowsByTitle)};
+    let control = document.querySelector('.sort-by-title')
+    if (control) {control.addEventListener('click', sortRowsByTitle)}
     })
+
 function sortRowsByTitle() {
     let table = document.querySelector('table')
     let rows = table.querySelectorAll('tr')
     let sortedRows = []
-    for (let i = 1; i <rows.length; i++ ){
-      sortedRows.push(rows[i])
+
+    for (let i = 1; i < rows.length; i++) {
+        sortedRows.push(rows[i])
     }
 
-    var arrowUp = this.querySelector('.octicon-arrow-up')
-    console.log(arrowUp)
+    let arrowUp = document.querySelector('.octicon-arrow-up')
+    let arrowDown = document.querySelector('.octicon-arrow-down')
 
-    sortedRows.sort(compareRowsAsc)
 
-    //if (){sortedRows.sort(compareRowsAsc)}
-    //else {sortedRows.sort(compareRowsDesc)}
+    console.log(arrowUp.classList)
+
+    if (arrowUp.classList.contains('hide')) {
+        sortedRows.sort(compareRowsAsc)
+        arrowUp.classList.remove('hide')
+        arrowDown.classList.add('hide')
+    } else {
+        sortedRows.sort(compareRowsDesc)
+        arrowDown.classList.remove('hide')
+        arrowUp.classList.add('hide')
+    }
+
 
     let sortedTable = document.createElement('table')
     sortedTable.classList.add('table')
     sortedTable.appendChild(rows[0])
-    for ( i=0; i < sortedRows.length; i++){
-        sortedTable.appendChild(sortedRows[i])
 
-        table.parentNode.appendChild(sortedTable, table)
+    for (i = 0; i < sortedRows.length; i++) {
+        sortedTable.appendChild(sortedRows[i])
     }
 
-    function compareRowsAsc(row1, row2){
+    table.parentNode.replaceChild(sortedTable, table)
+}
+
+
+function compareRowsAsc(row1, row2){
         let testTitle1 = row1.querySelector('td').textContent
         let testTitle2 = row2.querySelector('td').textContent
 
         if (testTitle1 < testTitle2) { return -1 }
         if (testTitle1 > testTitle2) { return 1 }
         return 0
+        }
 
-    }
-    function compareRowsDesc(row1, row2){
+function compareRowsDesc(row1, row2){
         let testTitle1 = row1.querySelector('td').textContent
         let testTitle2 = row2.querySelector('td').textContent
 
         if (testTitle1 < testTitle2) { return 1 }
         if (testTitle1 > testTitle2) { return -1 }
         return 0
-
-    }
-}
+        }
