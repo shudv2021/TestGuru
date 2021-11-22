2b3d77d3feb0bfe02d99aba5cf2446edd7feb131
shudv2021 <shudv@yandex.ru>
2021-11-22 08:21:57 +0300

    correction2

--- app/javascript/packs/utilites/timer.js
+++ app/javascript/packs/utilites/timer.js
@@ -1,15 +1,20 @@
 
 document.addEventListener('turbolinks:load', function(){
     let timerPanel = document.querySelector('.timer')
-
+    console.log(timerPanel)
     let remainingTime = timerPanel.dataset.remainingTime;
-    startTimer(remainingTime);
+    console.log(remainingTime)
+    startTimer(timerPanel, remainingTime);
 })
 
 function startTimer(timerPanel, remainingTime) {
     if (remainingTime > 0) {
+        console.log(remainingTime)
         timerPanel.textContent = remainingTime;
         remainingTime--;
-        setTimeout(startTimer, 1000, remainingTime)
-    } else {timerPanel.textContent = 'Завершить тест'}
+        TimerPanel.dataset.remainingTime = remainingTime;
+        setTimeout(startTimer, 1000, timerPanel, remainingTime)
+    } else {timerPanel.textContent = 'Завершить тест';
+        console.log('Завершить тест')
+    }
 }
